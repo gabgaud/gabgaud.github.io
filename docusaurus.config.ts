301fe047337986bcb63752f3568139b8dc57375d
gabgaud <93156290+gabgaud@users.noreply.github.com>
2024-08-06 13:55:50 -0400

    modified:   docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://www.tonprof.ca',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/intro',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
