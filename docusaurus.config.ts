d8f10416409a4d02afebc6f279b55b15650bb2f5
Gabriel Gaudreault <gabriel.gaudreault@hotmail.com>
2025-10-20 13:37:37 -0400

    new file:   docs/Infra/15 - 15 - Travail perso + Labo/00-cours15.tmp 	new file:   docs/Infra/15 - 15 - Travail perso + Labo/01-Laboratoire13.tmp 	new file:   docs/Infra/Assets/15/CreateMDS.png 	modified:   docusaurus.config.ts 	modified:   package-lock.json 	modified:   package.json

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,7 +2,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
-
+const math = require('remark-math');
+const katex = require('rehype-katex');
 const config: Config = {
   title: 'Le site de ton prof',
   tagline: 'L\'informatique simplifié',
@@ -55,6 +56,8 @@ const config: Config = {
               'cadenas',
             ],
           },
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
         },
         blog: false,
 
@@ -63,6 +66,15 @@ const config: Config = {
         },
       } satisfies Preset.Options,
     ],
+  ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity:
+        'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
   ],
   markdown: {
     mermaid: true,
